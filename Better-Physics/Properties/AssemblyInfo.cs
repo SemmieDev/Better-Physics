58bc24cd6b4eb4351a089742fbaf5b5889003fad
SemmieDev <semvdmeij124@gmail.com>
2024-12-10 17:15:53 +0100

    Bump version

--- Better-Physics/Properties/AssemblyInfo.cs
+++ Better-Physics/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(BetterPhysics.BetterPhysics), "BetterPhysics", "1.0.0", "SemmieDev")]
+[assembly: MelonInfo(typeof(BetterPhysics.BetterPhysics), "BetterPhysics", "1.0.1", "SemmieDev")]
 [assembly: MelonGame("OP Softworks", "Clamb")]
