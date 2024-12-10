d305adc9fa17f6d2f3ddbe7353fe603b5cdd0c6d
SemmieDev <semvdmeij124@gmail.com>
2024-12-10 17:06:27 +0100

    Remove ability to change fixed timestep

--- Better-Physics/BetterPhysics.cs
+++ Better-Physics/BetterPhysics.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 namespace BetterPhysics;
 
 public class BetterPhysics : MelonMod {
-    private MelonPreferences_Entry<float> fixedTimestep = null!;
-
     public override void OnInitializeMelon() {
         var category = MelonPreferences.CreateCategory("BetterPhysics");
 
-        fixedTimestep = category.CreateEntry("FixedTimestep", Time.fixedDeltaTime);
         Physics.defaultSolverIterations = category.CreateEntry("DefaultSolverIterations", Physics.defaultSolverIterations).Value;
         Physics.defaultSolverVelocityIterations = category.CreateEntry("DefaultSolverVelocityIterations", Physics.defaultSolverVelocityIterations).Value;
     }
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
-        Time.fixedDeltaTime = fixedTimestep.Value;
-
         if (sceneName != "Hike_V2" && sceneName != "Hike_V2_Demo") return;
 
         GameObject.FindWithTag("Character Controller").GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
